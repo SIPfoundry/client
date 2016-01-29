d4c0cb056c40e7abfa5ee6b44b96ce5d7e6f2cf2
kingservice <shengguojin2014@gmail.com>
2016-01-29 09:16:40 +0800

    change file structure

--- client/martein.js
+++ client/martein.js
@@ -2,33 +2,8 @@
 
 if (Meteor.isClient) {
 
- 
 
-  // This code only runs on the client
 
-  angular.module('simple-todos',['angular-meteor']);
-
- 
-
-  angular.module('simple-todos').controller('TodosListCtrl', ['$scope',
-
-    function ($scope) {
-
- 
-
-      $scope.tasks = [
-
-        { text: 'This is task 1' },
-
-        { text: 'This is task 2' },
-
-        { text: 'This is task 3' }
-
-      ];
-
- 
-
-  }]);
 
 }
 
